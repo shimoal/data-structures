063c8e52c8f952ec0f75d6a959472167a765ee2e
shimoal <shimoalmail@gmail.com>
2016-12-14 15:47:48 -0800

    Finish all stack patterns

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.sizeOfStack = 0;
 };
 
+Stack.prototype = {
+  push: function(value) {
+    this.sizeOfStack++;
+    this[this.sizeOfStack] = value;
+  },
+  pop: function() {
+    var popped = this[this.sizeOfStack];
+    delete this[this.sizeOfStack];
+    if (this.sizeOfStack > 0) {
 
+      this.sizeOfStack--;
+
+    }
+    return popped;
+  },
+  size: function() {
+    return this.sizeOfStack;
+  }
+};
+
+Stack.prototype.constructor = Stack;
