6134f51ccb74677f8047cc6426a8b3acbb80e238
shimoal <shimoalmail@gmail.com>
2016-12-14 14:39:05 -0800

    complete test 1-4

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var sizeOfStack = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    sizeOfStack++;
   };
 
   someInstance.pop = function() {
+    sizeOfStack--;
   };
 
   someInstance.size = function() {
+    return sizeOfStack;
   };
 
   return someInstance;
