8320574912078408b2b5b61b33d14570b7ea10fd
shimoal <shimoalmail@gmail.com>
2016-12-15 11:41:51 -0800

    Finish linkenkedList and pass all pre-written tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      list.tail = Node(value);
+      if (list.head === null) {
+        list.head = list.tail;
+      }
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var removedVal = list.head.value;
+    list.head = list.head.next;
+    return removedVal;
   };
 
   list.contains = function(target) {
+    var findTarget = function(node) {
+      node = node || list.head;
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return findTarget(node.next);
+    };
+    return findTarget();
   };
 
   return list;
@@ -26,4 +49,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail = O(1)
+ removeHead = O(1)
+ contains = O(n)
  */
