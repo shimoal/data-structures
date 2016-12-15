8c8caa5f15c69e45f085aa931dfdca807a4f3c47
shimoal <shimoalmail@gmail.com>
2016-12-14 16:41:01 -0800

    Pass all tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+  storage.sizeOfQueue = 0;
+  storage.highestIndex = 0;
+  storage.lowestIndex = 0;
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.sizeOfQueue++;
+    this[this.highestIndex] = value;
+    this.highestIndex++;
+  },
+  dequeue: function() {
+    if (this.sizeOfQueue > 0) {
+      this.sizeOfQueue--;
+      var dequeued = this[this.lowestIndex];
+      delete this[this.lowestIndex];
+      this.lowestIndex++;
+      return dequeued;
+    }
+  },
+  size: function() {
+    return this.sizeOfQueue;
+  }
+};
 
 
